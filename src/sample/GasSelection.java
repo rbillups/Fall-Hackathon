af2c96152063d7708cd7947da0e429795c1c8ef9
rbillups <reginaldbillups45@gmail.com>
2022-09-17 08:29:38 -0400

    Gallons functionallity added

--- src/sample/GasSelection.java
+++ src/sample/GasSelection.java
@@ -6,18 +6,47 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class GasSelection {
 
 
     @FXML
     private Button payment;
+    @FXML
+    private TextField prepayAmount;
+
+    @FXML
+    private TextField estGallons;
+
+    boolean eightSeven=false;
+    boolean eightNine=false;
+    boolean nineThree=false;
+    float total;
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    public void submit(ActionEvent event){
+        total= Float.parseFloat(prepayAmount.getText());
+        if(eightSeven==true){
+            total=total/3.09f;
+        }else if(eightNine==true){
+            total=total/3.64f;
+        }else{
+            total=total/4.19f;
+
+        }
+       String ans=String.valueOf(total);
+
+        estGallons.setText(ans);
+    }
 
     @FXML
     private void handlePayButton(ActionEvent event) throws IOException{
@@ -27,5 +56,25 @@ public class GasSelection {
         primaryStage.setScene(scene);
         ((Stage) payment.getScene().getWindow()).close();
         primaryStage.show();
+    }
+
+    public void eightySevenSelected(){
+         eightSeven=false;
+         eightNine=false;
+         nineThree=false;
+        eightSeven=true;
+    }
+    private void eightyNineSelected(){
+        eightSeven=false;
+        eightNine=false;
+        nineThree=false;
+        eightNine=true;
+    }
+    private void ninetyThreeSelected(){
+
+        eightSeven=false;
+        eightNine=false;
+        nineThree=false;
+        nineThree=true;
     }
 }

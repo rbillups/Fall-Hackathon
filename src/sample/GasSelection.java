abd26592804503f4866a84a724df63fb59df032b
tnfouce <Fo06052002>
2022-09-17 06:08:34 -0400

    latest

--- src/sample/GasSelection.java
+++ src/sample/GasSelection.java
@@ -1,4 +1,33 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class GasSelection {
+
+    @FXML
+    private ToggleButton eightySeven;
+
+    @FXML
+    private ToggleButton eightyNine;
+
+    @FXML
+    private ToggleButton ninetyThree;
+
+    @FXML
+    private void handleEightySevenButton(ActionEvent event) throws IOException {
+
+        eightySeven.setStyle("-fx-border-color: #ff0000; -fx-border-width: 5px;");
+
+    }
 }

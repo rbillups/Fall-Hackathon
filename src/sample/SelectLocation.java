c33d20a936e9c67496f9c465f0d76a0fcea5eefb
tnfouce <Fo06052002>
2022-09-17 05:59:29 -0400

    latest

--- src/sample/SelectLocation.java
+++ src/sample/SelectLocation.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class SelectLocation {
+
+    @FXML
+    private Button select1;
+
+    @FXML
+    private void handleSelectButton(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample/gasSelection.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = new Stage(StageStyle.DECORATED);
+        primaryStage.setScene(scene);
+        ((Stage) select1.getScene().getWindow()).close();
+        primaryStage.show();
+    }
 }
